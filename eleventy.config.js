83093a8fa817aa058b9dcc861e530a1617cacca9
Dean Miley <dean.miley@gov.scot>
2025-07-07 12:11:32 +0100

    Added in packages

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,7 +4,13 @@ export default function(eleventyConfig) {
   // Register the plugin
   eleventyConfig.addPlugin(govukEleventyPlugin, {
     // Optional: Specify the path to the GOV.UK Frontend assets
+    header: {
+      productName: 'SG Identity Architecture Documentation',
+      organisationName: 'Scottish Government',
+      organisationLogo: false
+    }
   })
+  eleventyConfig.addPassthroughCopy('app/assets/css')
 
   return {
     dataTemplateEngine: 'njk',
